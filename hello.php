95d1356f704031d8fb27bffd7445c38357a25f49
trankhai81 <138662958+trankhai81@users.noreply.github.com>
2023-07-13 10:52:01 +0700

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Under Maintenance</title>
+    <style>
+        body {
+            background-image: url('pink_lotus.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+        
+        .maintenance-message {
+            text-align: center;
+            margin-top: 200px;
+            font-size: 24px;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <div class="maintenance-message">
+        <h1>Currently the page is under maintenance</h1>
+    </div>
+</body>
+</html>
